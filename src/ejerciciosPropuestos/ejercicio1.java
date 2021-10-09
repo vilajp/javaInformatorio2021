7eb0cf7339f7b743204531839051bc9b4138c705
vilajp <vilajp@gmail.com>
2021-10-08 22:38:21 -0300

    se realizo ejercicio propuesto 1

---
+++ src/ejerciciosPropuestos/ejercicio1.java
@@ -0,0 +1,15 @@
+package ejerciciosPropuestos;
+
+public class ejercicio1{
+    public static void main(String[] args){
+        int numero1, numero2;
+        numero1 = 3;
+        numero2 = 4;
+        System.out.println(suma(numero1, numero2));
+    }
+
+    public static int suma(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+}
+
