2fa07858913adae77247452b9b1b8be902fbdba4
vilajp <vilajp@gmail.com>
2021-10-12 22:13:10 -0300

    terminado ejercicio2 level2

--- src/ejerciciosComplementariosLevel2/ejercicio2.java
+++ src/ejerciciosComplementariosLevel2/ejercicio2.java
@@ -5,6 +5,44 @@ después de agregar a en la primera y última posición).
 */
 
 package ejerciciosComplementariosLevel2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Ingrese cinco numero enteros");
+        Scanner teclado = new Scanner(System.in);
+
+        List<Integer> numeros = new ArrayList<>();
+        List<Integer> numerosAImprimir = new ArrayList<>();
+
+
+        for (int vueltas = 1; vueltas <= 5; vueltas++){
+            System.out.println("Ingrese Nro. para posicion "+vueltas);
+            Integer unNumero = teclado.nextInt();
+            numeros.add(unNumero);
+        }
+
+        System.out.println("Ingrese Nro. para la primera posicion");
+        Integer numeroPrimeraPosicion = teclado.nextInt();
+
+        System.out.println("Ingrese Nro. para la ultima posicion");
+        Integer numeroUltimaPosicion = teclado.nextInt();
+
+        numeros.add(numeroUltimaPosicion);
+        numerosAImprimir.add(numeroPrimeraPosicion);
+
+        for(Integer cadaNumero:numeros){
+            numerosAImprimir.add(cadaNumero);
+        }
+
+
+        for (Integer cadaNumero:numerosAImprimir){
+            System.out.println("#" + (numerosAImprimir.indexOf(cadaNumero)+1)+" - "+cadaNumero);
+
+
+        }
+
+    }
 }
