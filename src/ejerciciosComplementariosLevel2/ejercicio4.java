1863ecbebc83e97b1ed31cd5f29ab0496293dee6
vilajp <vilajp@gmail.com>
2021-10-13 20:59:27 -0300

    subo ejercicio4 level2

--- src/ejerciciosComplementariosLevel2/ejercicio4.java
+++ src/ejerciciosComplementariosLevel2/ejercicio4.java
@@ -7,6 +7,49 @@ Ayuda: ArrayList posee un método para particionar en sub-listas?
 */
 
 package ejerciciosComplementariosLevel2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ejercicio4 {
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese el nombres de 12 estudiantes");
+        Scanner teclado = new Scanner(System.in);
+
+        List<String> nombresEstudiantes = new ArrayList<>();
+        List<String> curso1 = new ArrayList<>();
+        List<String> curso2 = new ArrayList<>();
+        List<String> curso3 = new ArrayList<>();
+
+
+        for (int cadaEstudiante = 1; cadaEstudiante <= 12; cadaEstudiante++) {
+            System.out.println("Ingrese Nro. para estudiante Nro " + cadaEstudiante);
+            String unEstudiante = teclado.nextLine();
+            nombresEstudiantes.add(unEstudiante);
+        }
+
+        int cantidadEstudiantePorCurso = nombresEstudiantes.size() / 3;
+        for (String cadaEstudiante : nombresEstudiantes) {
+            if (nombresEstudiantes.indexOf(cadaEstudiante)+1 <= cantidadEstudiantePorCurso) {
+                curso1.add(cadaEstudiante);
+            } else if (nombresEstudiantes.indexOf(cadaEstudiante)+1 <= cantidadEstudiantePorCurso * 2) {
+                curso2.add(cadaEstudiante);
+            } else {
+                curso3.add(cadaEstudiante);
+            }
+        }
+        System.out.println("\nAlumnos Curso 1\n");
+        for (String cadaAlumno : curso1) {
+            System.out.println(cadaAlumno);
+        }
+        System.out.println("\nAlumnos Curso 2\n");
+        for (String cadaAlumno : curso2) {
+            System.out.println(cadaAlumno);
+        }
+        System.out.println("\nAlumnos Curso 3\n");
+        for (String cadaAlumno : curso3) {
+            System.out.println(cadaAlumno);
+        }
+    }
 }
