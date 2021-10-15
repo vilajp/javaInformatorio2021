15523971aec8bf89acb6fc62556878a0313c9d76
vilajp <vilajp@gmail.com>
2021-10-15 00:14:36 -0300

    terminado ejercicio7 de level2

--- src/ejerciciosComplementariosLevel2/ejercicio7.java
+++ src/ejerciciosComplementariosLevel2/ejercicio7.java
@@ -17,6 +17,46 @@ Crear una función que dado 2 argumentos (int, siendo el primero menor al segund
 
 
 package ejerciciosComplementariosLevel2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ejercicio7 {
+    public static void main(String[] args) {
+        List<String> fizzBuzz = new ArrayList<>();
+        Scanner teclado = new Scanner(System.in);
+        int sigo = 1;
+        String mensaje = "";
+        int valorInicial = 0;
+        int valorFinal = 0;
+        while(sigo == 1) {
+            System.out.println(mensaje);
+            System.out.println("Ingrese valor inicial de la funcion");
+            valorInicial = teclado.nextInt();
+            System.out.println("Ingrese valor final de la funcion");
+            valorFinal = teclado.nextInt();
+            if(valorFinal<=valorInicial) {
+                mensaje = "El valor inicial debe ser menor que el valor final, intente nuevamente...";
+            }else{
+                sigo = 0;
+            }
+        }
+        fizzBuzz = fizzBuzzfuncion(valorInicial,valorFinal);
+        System.out.println(fizzBuzz);
+    }
+    public static List fizzBuzzfuncion(int valorInicial, int valorFinal){
+        List<String> resultado = new ArrayList<>();
+        for (int incremento=valorInicial; incremento<valorFinal; incremento++){
+            if (incremento%2==0&incremento%3==0){
+                resultado.add("FizzBuzz");
+            }else if(incremento%2==0){
+                resultado.add("Fizz");
+            }else if(incremento%3==0){
+                resultado.add("Buzz");
+            }else {
+                resultado.add(String.valueOf(incremento));
+            }
+        }
+        return resultado;
+    }
 }
